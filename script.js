6f05436d31d8f376566a75faae5431f846e38d50
Louise Krejler <loui47@stud.kea.dk>
2023-01-07 13:08:08 +0100

    smooth scroll og reveal

---
+++ script.js
@@ -0,0 +1,29 @@
+/* reveal */
+window.addEventListener("scroll", function () {
+  const revealElements = document.querySelectorAll(".scroll .reveal");
+  revealElements.forEach((revealElement) => {
+    if (isElementInViewport(revealElement)) {
+      revealElement.classList.add("revealed");
+    }
+  });
+});
+
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+}
+
+/* smooth scrolling */
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    smoothScrollTo(link.getAttribute("href").substring(1));
+  });
+});
+
+function smoothScrollTo(elementId) {
+  const element = document.getElementById(elementId);
+  element.scrollIntoView({ behavior: "smooth" });
+}
