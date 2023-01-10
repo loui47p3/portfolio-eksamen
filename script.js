c75780cb637b6410e1944938773a6115857d8dd5
Louise Krejler <loui47@stud.kea.dk>
2023-01-10 02:28:01 +0100

    hamburger menu

--- script.js
+++ script.js
@@ -28,12 +28,38 @@ function smoothScrollTo(elementId) {
   element.scrollIntoView({ behavior: "smooth" });
 }
 
-/* menu */
-const button = document.querySelector(".menu_button");
-const menu = document.querySelector(".menu");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-button.addEventListener("click", () => {
-  menu.style.display = menu.style.display === "none" ? "block" : "none";
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  bar1.classList.toggle("active");
+  bar2.classList.toggle("active");
+  bar3.classList.toggle("active");
 });
 
-/* aktiv menu */
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 200;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
