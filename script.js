28f86dc8f56cf671e285b040d0731eb265d72f15
Louise Krejler <loui47@stud.kea.dk>
2023-01-10 22:24:36 +0100

    ny billeder

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* reveal */
+/* scroller = reveal */
 window.addEventListener("scroll", function () {
   const revealElements = document.querySelectorAll(".scroll .reveal");
   revealElements.forEach((revealElement) => {
@@ -8,6 +8,7 @@ window.addEventListener("scroll", function () {
   });
 });
 
+/* revealed */
 function isElementInViewport(el) {
   const rect = el.getBoundingClientRect();
   return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
@@ -21,6 +22,7 @@ const bar1 = document.querySelector(".bar1");
 const bar2 = document.querySelector(".bar2");
 const bar3 = document.querySelector(".bar3");
 
+/* click = toggle active class + vis bar1,2,3 */
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
@@ -29,23 +31,10 @@ hamburger.addEventListener("click", () => {
   bar3.classList.toggle("active");
 });
 
+/* click på link = fjern active */
 document.querySelectorAll(".nav-link").forEach((n) =>
   n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
   })
 );
-
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 200;
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
